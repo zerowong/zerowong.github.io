680d931f88efdefbb369e0c8056302c5fcfbd8c0
zerowong <wongzero@foxmail.com>
2021-02-06 15:14:46 +0800

    feat: add gugrd of backstage route('/manager')

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../pages/Home.vue'
+import { Notification } from 'element-ui'
+import store from '../store'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/blog',
-    name: 'Blog',
-    component: () => import('@/pages/Blog.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('@/pages/About.vue'),
-  },
-  {
-    path: '/manager',
-    component: () => import('@/pages/Manager.vue'),
-  },
-  {
-    path: '*',
-    component: () => import('@/pages/NotFound.vue'),
-  },
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/pages/Home.vue'),
+    },
+    {
+      path: '/blog',
+      component: () => import('@/pages/Blog.vue'),
+    },
+    {
+      path: '/about',
+      component: () => import('@/pages/About.vue'),
+    },
+    {
+      path: '/manager',
+      component: () => import('@/pages/Manager.vue'),
+      beforeEnter(to, from, next) {
+        if (store.state.user?.role === 'admin') {
+          next()
+        } else {
+          next(new Error('你没有权限'))
+        }
+      },
+    },
+    {
+      path: '*',
+      component: () => import('@/pages/NotFound.vue'),
+    },
+  ],
+})
+
+router.onError((err) => {
+  Notification({ type: 'error', message: err.message, position: 'bottom-right' })
 })
 
 export default router
