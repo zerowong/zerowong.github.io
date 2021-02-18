d2e15aa77258bf8b664150db4a8718cf7c26ce1a
zerowong <wongzero@foxmail.com>
2021-02-18 12:38:13 +0800

    chore: remove ‘webpack-bundle-analyzer’ * because it’s built-in in ‘@vue/cli-service’

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
-const plugins = [new MomentLocalesPlugin({ localesToKeep: ['zh-cn'] })]
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }))
-}
 
 module.exports = {
   lintOnSave: false,
-  configureWebpack: { plugins },
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [new MomentLocalesPlugin({ localesToKeep: ['zh-cn'] })],
+  },
 }
