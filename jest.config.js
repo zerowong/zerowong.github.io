3eb967b32d8a38235cb9e2a45ce67ee95896d570
zerowong <wongzero@foxmail.com>
2021-03-03 00:10:21 +0800

    clean up

--- jest.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { preset: '@vue/cli-plugin-unit-jest' }
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
-
-module.exports = {
-  lintOnSave: false,
-  productionSourceMap: false,
-  configureWebpack: {
-    plugins: [new MomentLocalesPlugin({ localesToKeep: ['zh-cn'] })],
-  },
-}
